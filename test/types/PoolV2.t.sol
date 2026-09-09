98a81cd0b2397dc647ac70ed1b90c0b9ae445afe
JMSBPP <jmsbpp@hotmail.es>
2026-09-09 09:38:40 -0400

    test: imp reactive-test-lib, setter getter for v4

--- test/types/PoolV2.t.sol
+++ test/types/PoolV2.t.sol
@@ -2,53 +2,52 @@
 pragma solidity ^0.8.26;
 
 import {PlankTestBase} from "test/PlankTestBase.sol";
+// reactive-test-lib/=lib/reactive-test-lib/src/
+import {ReactiveTest} from "reactive-test-lib/ReactiveTest.sol";
+import {ReactiveConstants} from "reactive-test-lib/constants/ReactiveConstants.sol";
+import {Deployers} from "v4-core-test/utils/Deployers.sol";
 
-interface IPoolV2 {
-    function setChainId(uint256 chainId) external;
-    function readChainId() external view returns (uint256);
+
+
+interface IUniswapV4PoolReactive {
+    function setSystemContract(address) external;
+    function getSystemContract() external returns(uint256);
 }
-
-/// @title PoolV2Test
-/// @notice Dual-fork PoolV2Harness: set on A, assert isolation on B, read back on A.
-contract PoolV2Test is PlankTestBase {
-    IPoolV2 internal harnessA;
-    IPoolV2 internal harnessB;
+contract PoolV2Test is PlankTestBase, ReactiveTest, Deployers {
     uint256 internal forkA;
     uint256 internal forkB;
-    uint256 internal recordedIdA;
-    uint256 internal recordedIdB;
+    uint256 internal forkReactive;
+    address uniswapV4PoolReactive;
 
-    function setUp() public {
+    function setUp() public override {
+	super.setUp();
         string[] memory cmd = new string[](2);
         cmd[0] = "just";
         cmd[1] = "init-chains";
         vm.ffi(cmd);
-
+ 
         forkA = vm.createFork(vm.rpcUrl("chainA"));
+	vm.selectFork(forkA);
+	deployFreshManager();
+	registerChain(address(manager),block.chainid);
+	
         forkB = vm.createFork(vm.rpcUrl("chainB"));
         assertTrue(forkA != forkB);
 
-        vm.selectFork(forkA);
-        harnessA = IPoolV2(deployPlank("test/harness/types/PoolV2Harness.plk"));
-        assertTrue(address(harnessA) != address(0));
-
-        vm.selectFork(forkB);
-        harnessB = IPoolV2(deployPlank("test/harness/types/PoolV2Harness.plk"));
-        assertTrue(address(harnessB) != address(0));
+	forkReactive = vm.createFork(vm.rpcUrl("chainReactive"));
+        vm.selectFork(forkReactive);
+	uniswapV4PoolReactive = deployPlank("src/modules/UniswapV4PoolReactive.plk");
+	assertTrue(address(uniswapV4PoolReactive) != address(0));
     }
 
-    function test__unit__poolChainId() public {
-        vm.selectFork(forkA);
-        recordedIdA = block.chainid;
-        harnessA.setChainId(recordedIdA);
-
-        vm.selectFork(forkB);
-        recordedIdB = block.chainid;
-        assertTrue(recordedIdB != recordedIdA);
-        assertTrue(recordedIdB != 0);
-        assertEq(harnessB.readChainId(), 0);
-
-        vm.selectFork(forkA);
-        assertEq(harnessA.readChainId(), recordedIdA);
+    function test__setSystemContract__success() public {
+	vm.selectFork(forkReactive);
+	IUniswapV4PoolReactive(uniswapV4PoolReactive).setSystemContract(address(sys));
+	address expectedSysAddress = address(ReactiveConstants.SERVICE_ADDR);
+	uint256 resSysAddress = IUniswapV4PoolReactive(uniswapV4PoolReactive).getSystemContract();
+	assertEq(expectedSysAddress,address(uint160(resSysAddress)));
+	
     }
+    
+
 }
